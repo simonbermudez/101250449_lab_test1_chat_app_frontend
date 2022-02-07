6548d0ad4c496e51a2292bac28bcd2e3fc9f6730
Simon Bermudez <sabdz55@gmail.com>
2022-02-07 14:37:06 -0500

    frontend based in socketchat

--- README.old.md
+++
@@ -1,2 +0,0 @@
-# 101250449_lab_test1_chat_app_frontend
-Front end Lab Test 1 Full Stack 2 George Brown College
--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
 
-function App() {
+import Chat from 'pages/chat/Chat';
+
+import Store from 'context/Store';
+
+import './App.scss';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Store>
+      <div className='App'>
+        <Router>
+          <Switch>
+            <Route path='/' component={Chat} />
+          </Switch>
+        </Router>
+      </div>
+    </Store>
   );
-}
+};
 
 export default App;
